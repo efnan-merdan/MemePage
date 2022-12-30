8eba2238168a7059ae6b64b8451edcf168b339b3
efnan-merdan <efnan.merdan@stu.ssst.edu.ba>
2022-12-30 21:11:11 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'; 
 import './App.css';
+import SearchIcon from './search.svg';
+import MemeList from './MemeList.json';
+import MemeCard from './MemeCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+console.log(MemeList);
+
+const App = () => {
+
+    const [searchTerm, setSearchTerm] = useState("");
+    const [memes, setMemes] = useState([]);
+
+    useEffect(() => {
+        searchMemes("Chickens");
+      }, []);
+
+      const searchMemes = async (title) => {
+        const response = await fetch("MemeList.json");
+        const data = await response.json();
+    
+        setMemes(data.Search);
+      };
+
+    console.log(memes.length);  
+
+    return (
+        <div className="app">
+            <h1>MemeLand</h1>
+ 
+            <div className="search">
+                <input 
+                    placeholder="Search for movie"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMemes(searchTerm)}
+                />
+            </div>
+
+            {memes?.length > 0 ? (
+                
+                <div className="container">
+                    {memes.map((meme) => (
+                        <MemeCard meme={meme}/>
+                    ))}   
+                </div>
+            ) : (
+                
+                <div className='empty'>
+                    <h2>No memes found</h2>
+                </div>
+            )}
+    
+        </div>
+    );
+    
 }
 
 export default App;
